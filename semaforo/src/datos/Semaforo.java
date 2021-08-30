f87ee0b338e121954abf5407ad1a269b270c849d
david camelo <dcamelos@unal.edu.co>
2021-08-30 02:40:45 -0500

    arreglos

--- semaforo/src/datos/Semaforo.java
+++ semaforo/src/datos/Semaforo.java
@@ -12,34 +12,35 @@ public class Semaforo {
 	public void AtravezarIntercepcion(Calle calle, long timeStamp) {
 		if (color=="verde"){
 			System.out.println("El semaforo" + this.orientation +
-				"Esta en color" + this.color +
-				" Carros comienzan a cruzar " + calle.getOrientacion() + 
+				" Esta en color " + this.color +
+				" Carros comienzan a cruzar " +  
 				" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
-				"seg");
-			for (int i = 0; i <= 5; i++) { 
+				" seg");
+			for (int i = 0; i < 5; i++) { 
 				this.esperarXsegundos(calle.getCarros()[i]);
 				System.out.println("Calle " + getOrientacion() + " Han cruzando " + (i + 1) +" carros " +  
 				" ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + 
-				"seg");
+				" seg");
 			}
+			
 		}
 			
 		else{
 			System.out.println("El semaforo" + this.orientation + 
-			"Esta en color"+ this.color+
-			" Carros estan parados " + calle.getOrientacion() + 
+			" Esta en color "+ this.color+
+			" Carros estan parados " +  
 			" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
-			"seg");
-			for (int i = 0; i <=5; i++) { 
+			" seg");
+			for (int i = 0; i <5; i++) { 
 				this.esperarXsegundos(calle.getCarros()[i]); 
 				System.out.println("Calle " + getOrientacion() + " Hay " + (i + 1) + " Carros parados " + 
 				" ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + 
-				"seg");
+				" seg");
 
 		}
 	}
 
-	System.out.println("LOS SEMAFOROS HAN CAMBIADO DE COLOR EN EL TIEMPO: " + 
+	System.out.println("EL SEMAFORO " + this.orientation+ " HA CAMBIADO DE COLOR EN EL TIEMPO: " + 
 	(System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 
 	if (color=="verde"){
@@ -49,9 +50,9 @@ public class Semaforo {
 		setColor("verde");
 	}
 	if (color=="verde"){
-		System.out.println("El semaforo" + this.orientation +
-			"Esta en color" + this.color +
-			" Carros comienzan a cruzar " + calle.getOrientacion() + 
+		System.out.println("El semaforo " + this.orientation +
+			" Esta en color" + this.color +
+			" Carros comienzan a cruzar " + 
 			" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
 			"seg");
 		for (int i = 5; i <10; i++) { 
@@ -63,12 +64,12 @@ public class Semaforo {
 	}
 		
 	else{
-		System.out.println("El semaforo" + this.orientation + 
-		"Esta en color"+ this.color+
-		" Carros estan parados " + calle.getOrientacion() + 
+		System.out.println("El semaforo " + this.orientation + 
+		" Esta en color "+ this.color+
+		" Carros estan parados " +  
 		" EN EL TIEMPO: " + (System.currentTimeMillis() - timeStamp) / 1000	+
 		"seg");
-		for (int i = 0; i<=5; i++) { 
+		for (int i = 0; i<10; i++) { 
 			this.esperarXsegundos(calle.getCarros()[i]); 
 			System.out.println("Calle " + getOrientacion() + " Hay " + (i + 1) + " Carros parados " +
 			" ->Tiempo: " + (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
@@ -76,7 +77,7 @@ public class Semaforo {
 	}
 
 }
-System.out.println("LOS SEMAFOROS HAN CAMBIADO DE COLOR EN EL TIEMPO: " + 
+System.out.println("EL SEMAFORO " + this.orientation+ " HA CAMBIADO DE COLOR EN EL TIEMPO: " + 
 (System.currentTimeMillis() - timeStamp) / 1000 + "seg");
 	}
 
